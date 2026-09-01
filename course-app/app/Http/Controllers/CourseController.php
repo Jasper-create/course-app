c87be48ec4e85c7dd393da251578d151cb82e85e
Jasper-create <sanchezianjasper79@gmail.com>
2026-09-01 12:21:40 +0800

    add CourseController, Model/ Course model and Migration
    ;
    
    .
    q
    \q

---
+++ course-app/app/Http/Controllers/CourseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    //
+    public function index(){
+        //List all
+    }
+
+    public function create(){
+        //routing to page/view whish will hold the create form
+    }
+
+    public function store(Request $request){
+        //This would create the enr for the course
+    }
+
+    public function show($id){
+        //fetching the data from the database
+    }
+
+    public function edit($id){
+        //edit data
+    }
+
+    public function update(Request $request, $iid){
+        //Logic to update the record in the database and do something post that
+    }
+
+    public function destroy($id){
+        //Delete data entry
+    }
+}
